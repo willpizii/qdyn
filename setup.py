c4daedf9e7e0a93712ef9b8e4aad71d9c923db17
Will Pizii <will@localhost.localdomain>
2024-07-16 22:26:59 +0100

    Included a meta.yaml file for direct management by conda, and updated setup.py to account for this. Fixed breaking typo in pyqdyn.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name="qdyn", version="3.0.0", packages=["qdyn"])
+setup(name="qdyn", version="3.0.0", packages=find_packages())
